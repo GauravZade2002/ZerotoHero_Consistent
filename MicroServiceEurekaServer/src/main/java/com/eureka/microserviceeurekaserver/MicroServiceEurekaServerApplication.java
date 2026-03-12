0650c8e008a4412dd6c51a66cfca5ad128fce866
GauravZade2002 <Kajal@*12345>
2026-03-12 23:50:29 +0530

    Eureka server integration in microservices

---
+++ MicroServiceEurekaServer/src/main/java/com/eureka/microserviceeurekaserver/MicroServiceEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.microserviceeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceEurekaServerApplication.class, args);
+    }
+
+}
