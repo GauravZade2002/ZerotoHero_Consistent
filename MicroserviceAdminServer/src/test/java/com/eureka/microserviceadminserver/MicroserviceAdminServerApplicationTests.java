2efd6eb4218c192874826f1d498c04a64d2af9a6
GauravZade2002 <Kajal@*12345>
2026-03-13 00:58:15 +0530

    Admin server set up for microservices

---
+++ MicroserviceAdminServer/src/test/java/com/eureka/microserviceadminserver/MicroserviceAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.microserviceadminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
